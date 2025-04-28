7ee9f2c2297dfeafea4e105327ac42b27d3e24e8
ju5nj0se <juaan.josehernandez@gmail.com>
2025-04-28 16:11:53 -0500

    Add files via upload

---
+++ Semana2/Modulo1-Semana2.py
@@ -0,0 +1,12 @@
+'''
+    1. ingresar una calificacion numerica y validar la entrada
+    2. ingresar una lista de calificaciones  y un valor especifico para comparar
+    3.  utilizar condicionales para determinar el estado de aprobacion 
+    4. utilizar un ciclo para contar cuantas calificaciones son mayores que el valor especificado
+    5. emplear un ciclo for para verificar la presencia de una calificacion especifica y contar cuantas veces aparece (utilizar break y continue)
+    
+
+
+
+
+'''
